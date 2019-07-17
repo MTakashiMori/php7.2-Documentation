217a77483b4af17fd46473758b3dcc776ed27a41
Marcelo <t-takashi@iabs.org.br>
2019-07-17 17:56:09 -0300

    improve in menu

--- documentation/referenciaDaLinguagem/sintaxeBasica.php
+++ documentation/referenciaDaLinguagem/sintaxeBasica.php
@@ -1,4 +1,3 @@
-<?php
-    echo '<h1> Sintaxe básica </h1>'
-        
-?>
+<h1>Sintaxe Básica</h1>
+
+A sintaxe base do <strong> PHP </strong> começa com a tag <strong>< ? php</strong> e termina com <strong> ?></strong>
--- index.php
+++ index.php
@@ -2,16 +2,24 @@
     $links = [
         [
             'name' => 'Home',
-            'route' => '/'
+            'route' => '/',
+            'children' => null
         ],
         [
             'name' => 'About',
-            'route' => '/about.php'
+            'route' => '/about.php',
+            'children' => null
         ],
         [
-            'name' => 'Sintaxe Basica',
-            'route' => 'documentation/referenciaDaLinguagem/sintaxeBasica.php'
-        ]    
+            'name' => 'Documentation',
+            'route' => '/index.php',
+            'children' => [
+                [
+                    'name' => 'Sintaxe Basica',
+                    'route' => 'documentation/referenciaDaLinguagem/sintaxeBasica.php'
+                ]
+            ]
+        ],    
     ];  
 
     echo '<ul>';
@@ -19,6 +27,18 @@
         echo '<li>';
         
         echo "<a href=". $link['route']  .">".   $link['name']  . '</a>';   
+
+        if($link['children']) {
+            foreach($link['children'] as $child) {
+
+            echo '<ul><li>';
+        
+            echo "<a href=". $child['route']  .">".   $child['name']  . '</a>';   
+
+            echo '</li></ul>';
+
+            }
+        }
 
         echo '</li>';
     }
